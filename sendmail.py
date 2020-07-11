1093218d491386c4029d91a3b1b5d4bd28bf0bac
Kapil mundra <55842126+Kapilmundra@users.noreply.github.com>
2020-07-11 07:31:36 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -3,7 +3,7 @@ print("Sending mail...")
 import smtplib
 s=smtplib.SMTP('smtp.gmail.com',587)
 s.starttls()
-s.login("mundrak21@gmail.com","mjipeehczuzmsxyh")
+s.login("mundrak21@gmail.com","tciehganbjmdoinx")
 message1="success"
 s.sendmail("mundrak21@gmail.com","mundrak21@gmail.com",message1)
 s.quit()
